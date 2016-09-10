6f287fb8ec0d997f9e5b58b38fe270253648e23d
Elsy Sugeli Pinzón Vivas <espinzonv@MacBook-Air-de-Elsy.local>
2016-09-10 10:48:19 -0500

    plantilla

---
+++ plantilla.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+
+//https://github.com/erosethan/ninja-ref
+//https://github.com/elsypinzonv/cpp_problems
+//g++ -std=c++11 your_file.cpp -o your_program
+//./a.out < archivo.in leer archivo
+//./a.out > archivo.out escupir en un archivo
+// ./a.out < archivo.in > archivo.out
+
+void solve(){
+
+}
+
+int main(){
+    optimizar_io
+    solve();
+    return 0;
+}
